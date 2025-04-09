2643cb2fc229634539caa69629138d8b1e2ec715
matdexir <matthieudesir21@gmail.com>
2025-04-09 21:26:47 +0800

    feat: get a proper functionning interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
-use std::{collections::VecDeque, io::Read};
+use std::{
+    collections::VecDeque,
+    io::{self, Read, Write},
+};
+
+const MEMORY_SIZE: usize = 10;
 
 #[derive(Debug)]
 pub struct BrainFuckVM {
     pc: usize,                 // Program counter
-    cell_pc: usize,            // Keeping Track of the current cell
+    cc: usize,                 // Keeping Track of the current cell
     instructions: Vec<OpCode>, // Program instructions
-    registers: [u8; 10],       // Program Registers
+    tape: [u8; MEMORY_SIZE],   // Program Registers
     // loop_start_stack: Vec<usize>,
     // loop_end_queue: VecDeque<usize>,
     #[cfg(debug_assertions)]
@@ -30,9 +35,9 @@ impl BrainFuckVM {
 
         BrainFuckVM {
             pc: 0,
-            cell_pc: 0,
+            cc: 0,
             instructions,
-            registers: [0; 10],
+            tape: [0; MEMORY_SIZE],
             // loop_start_stack: Vec::new(),
             // loop_end_queue: VecDeque::new(),
             #[cfg(debug_assertions)]
@@ -60,72 +65,74 @@ impl BrainFuckVM {
     }
 
     fn incr_pc(&mut self) {
-        self.pc += 1;
+        self.pc = self.pc.wrapping_add(1);
     }
 
     fn decr_pc(&mut self) {
-        self.pc -= 1;
+        self.pc = self.pc.wrapping_sub(1);
     }
 
     pub fn exec(&mut self) {
-        loop {
+        while self.pc < self.instructions.len() {
             if let Some(c) = &self.instructions.get(self.pc) {
                 #[cfg(debug_assertions)]
                 println!("c: {:?}", c);
 
                 match c {
                     OpCode::IncrementCell => {
-                        self.registers[self.cell_pc] += 1;
-                        self.incr_pc();
+                        self.tape[self.cc] = self.tape[self.cc].wrapping_add(1);
                     }
                     OpCode::DecrementCell => {
-                        self.registers[self.cell_pc] -= 1;
-                        self.incr_pc();
+                        self.tape[self.cc] = self.tape[self.cc].wrapping_sub(1);
                     }
                     OpCode::Increment => {
-                        self.cell_pc += 1;
-                        self.incr_pc();
+                        // self.cc = self.cc.wrapping_add(1);
+                        self.cc += 1
                     }
                     OpCode::Decrement => {
-                        self.cell_pc -= 1;
-                        self.incr_pc();
+                        // self.cc = self.cc.wrapping_sub(1);
+                        self.cc -= 1
                     }
                     OpCode::WriteCell => {
                         #[cfg(debug_assertions)]
                         {
-                            self.buffered_output += &self.registers[self.cell_pc].to_string()
+                            self.buffered_output += &(self.tape[self.cc] as char).to_string()
                         }
 
-                        print!("{:?}", self.registers[self.cell_pc]);
-                        self.incr_pc();
+                        let _ = io::stdout().write_all(&self.tape[self.cc..self.cc + 1]);
                     }
                     OpCode::ReadCell => {
-                        let mut input_buf = [0];
-                        let _ = std::io::stdin().read_exact(&mut input_buf);
-                        self.registers[self.cell_pc] = input_buf[0];
-                        self.incr_pc();
+                        let _ = io::stdin().read_exact(&mut self.tape[self.cc..self.cc + 1]);
                     }
                     OpCode::LoopStart => {
-                        if self.registers[self.cell_pc] == 0 {
-                            loop {
-                                let op_code = &self.instructions[self.pc];
-                                match op_code {
-                                    OpCode::LoopEnd => break,
-                                    _ => self.incr_pc(),
+                        if self.tape[self.cc] == 0 {
+                            let mut level = 1;
+                            while level > 0 {
+                                self.pc += 1;
+
+                                match self.instructions[self.pc] {
+                                    OpCode::LoopStart => level += 1,
+                                    OpCode::LoopEnd => level -= 1,
+                                    _ => {}
                                 }
                             }
                         }
-                        self.incr_pc();
                     }
-                    OpCode::LoopEnd => loop {
-                        let op_code = &self.instructions[self.pc];
-                        match op_code {
-                            OpCode::LoopStart => break,
-                            _ => self.decr_pc(),
+                    OpCode::LoopEnd => {
+                        if self.tape[self.cc] != 0 {
+                            let mut level = 1;
+                            while level > 0 {
+                                self.pc -= 1;
+                                match self.instructions[self.pc] {
+                                    OpCode::LoopStart => level -= 1,
+                                    OpCode::LoopEnd => level += 1,
+                                    _ => {}
+                                }
+                            }
                         }
-                    },
-                    _ => continue,
+                    }
                 }
+                self.incr_pc();
 
                 #[cfg(debug_assertions)]
                 self.inspect();
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use brainf::BrainFuckVM;
+use std::{env, fs, process};
 
 fn main() {
-    let content = String::from("++++[-]");
-    let mut vm = BrainFuckVM::new(content);
-    vm.exec();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <brainfuck-file>", args[0]);
+        process::exit(1);
+    }
+    let filename = &args[1];
+
+    match fs::read_to_string(filename) {
+        Ok(contents) => {
+            let mut vm = BrainFuckVM::new(contents);
+            vm.exec();
+        }
+        Err(error) => {
+            eprintln!("Error reading file '{}': {}", filename, error);
+            process::exit(1);
+        }
+    }
 }
